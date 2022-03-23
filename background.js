734f1ec670ef68fb402bb12c9da8042b7fdd8397
Gustavo Silva <me@gustavosilva.io>
2022-03-23 22:44:19 +0000

    First commit

---
+++ background.js
@@ -0,0 +1,36 @@
+importScripts('./securityRules.js');
+
+let activeTabId, previousUrl, lastUrl, lastTitle;
+
+function runChromeScript(tabId){
+    chrome.scripting.executeScript({
+        target: {
+            tabId: tabId
+        },
+        function: applySecurityRules,
+    });
+}
+
+function getTabInfo(tabId) {
+	chrome.tabs.get(tabId, function(tab) {
+		if (lastUrl != tab.url || lastTitle != tab.title) {
+            lastUrl = tab.url;
+            if(lastUrl !== previousUrl) {
+                console.log('🔎 ' + lastUrl);
+            }
+            previousUrl = lastUrl;
+        }
+        runChromeScript(tabId);
+	});
+}
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	getTabInfo(activeTabId = activeInfo.tabId);
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	// if (activeTabId == tabId) {
+	// 	getTabInfo(tabId);
+	// }
+    getTabInfo(tabId);
+});
---
+++ securityRules.js
@@ -0,0 +1,31 @@
+const applySecurityRules = () => {
+
+	//AC: As an Origin Employee I shouldn't be able to access the /users feature on LaunchDarkly
+	const launchDarklyUsers = () => {
+
+        //LaunchDarkly not opened
+        if(window.location.hostname.indexOf('launchdarkly') == -1) return;
+
+        const removeButtons = () => {
+            var btnsUsersPage = document.querySelectorAll("a[href='/default/production/users']");
+            btnsUsersPage.forEach(btn => btn.remove());
+        }
+
+        // Remove Buttons immediately
+        removeButtons();
+
+        // Remove Buttons after page is loaded
+        window.addEventListener("DOMContentLoaded", function() {
+            removeButtons();
+        });
+
+        //Blocks manual navigation
+        if(window.location.pathname.indexOf('default/production/users') != -1){
+            window.location.replace("https://app.launchdarkly.com");
+        }
+
+    }
+
+    launchDarklyUsers();
+    
+}
